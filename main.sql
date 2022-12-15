f06217d7986febc3029a007248907923b6b12bbe
Рифат Бахтиев <rifatbakhtiev@gmail.com>
2022-12-15 13:28:15 +0400

    New commit

--- main.sql
+++ main.sql
@@ -1,12 +1,10 @@
 create table if not exists Genre (
 	id serial primary key,
-	genre_id INTEGER not null references Genre(id),
 	name VARCHAR(40) NOT NULL
 );
 
 create table if not exists Singer (
 	id serial primary key,
-	singer_id INTEGER not null references Singer(id),
 	name VARCHAR(40) NOT NULL
 );
 
@@ -18,9 +16,8 @@ create table if not exists GenreSinger (
 
 create table if not exists Album (
 	id serial primary key,
-	album_id INTEGER not null references Album(id),
 	name VARCHAR(40) NOT null,
-	year_of_issue VARCHAR(40) NOT null
+	year_of_issue DATE NOT null
 );
 
 create table if not exists SingerAlbum (
@@ -30,17 +27,17 @@ create table if not exists SingerAlbum (
 );
 
 create table if not exists Track (
-	id INTEGER primary key references Album(id),
-	track_id INTEGER not null references Track(id),
-	year_of_issue VARCHAR(80),
-	duration VARCHAR(80)
+	id serial primary key,
+	album_id INTEGER not null references Album(id),
+	name VARCHAR(40) NOT null,
+	year_of_issue DATE,
+	duration TIME
 );
 
 create table if not exists Compilation (
 	id serial primary key,
-	compilation_id INTEGER not null references Compilation(id),
 	name VARCHAR(40) NOT null,
-	year_of_issue VARCHAR(40) NOT null
+	year_of_issue DATE NOT null
 );
 
 create table if not exists TrackCompilation (
@@ -49,4 +46,4 @@ create table if not exists TrackCompilation (
 	constraint TC primary key (track_id, compilation_id)
 );
 
----- DROP TABLE Genre, Singer, Album, GenreSinger, SingerAlbum;
+-- DROP TABLE Album, SingerAlbum, Track, Genre, Singer, GenreSinger, Compilation, TrackCompilation;
